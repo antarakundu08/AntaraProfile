144c524626ac7c9f71a16c6f6d8811d5857a53fc
Antara Kundu <61645954+antarakundu08@users.noreply.github.com>
2024-05-17 19:37:40 +0000

    configured tailwind and added assets

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import ants from '../assets/ants.jpeg'
 const About = () => {
   return (
-    <div className=''>
-      About
+    <div className='pt-[10%] px-10 flex'>
+      <div>
+        <img src={ants} alt="userIcon" />
+      </div>
+      <div>
+        <p>
+        Focused and enthusiastic developer with a keen interest in software development and artificial intelligence. By comprehensive exposure to the underlying concepts and applying them vividly to various projects, my love for these domains came into being. I am a passionate individual who thrives to build and apply algorithms to solve real-world industry problems.
+        </p>
+      </div>
     </div>
   )
 }
